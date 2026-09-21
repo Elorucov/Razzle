00e3d7b9587aecb47713790c587c8f6d5cecc403
Elchin Orujov <elorucov@live.ru>
2026-09-21 15:36:23 +0400

    Add project files.

---
+++ backend/Program.cs
@@ -0,0 +1,15 @@
+namespace ELOR.Razzle
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
